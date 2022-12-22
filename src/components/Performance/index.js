5e81f0a1e6e8066ae0c3672837989b8db44c7e9e
Roger Retita <roger.retita@gmail.com>
2022-12-22 18:24:33 +0100

    Modification

--- src/components/Performance/index.js
+++ src/components/Performance/index.js
@@ -8,60 +8,60 @@ import {
   PolarRadiusAxis
 } from "recharts";
 
-var kinds = {
-  "1": "cardio",
-  "2": "energy",
-  "3": "endurance",
-  "4": "strength",
-  "5": "speed",
-  "6": "intensity"
-};
+// var kinds = {
+//   "1": "cardio",
+//   "2": "energy",
+//   "3": "endurance",
+//   "4": "strength",
+//   "5": "speed",
+//   "6": "intensity"
+// };
 
-const data = [
-  {
-    value: 200,
-    kind: 1
-  },
-  {
-    value: 240,
-    kind: 2
-  },
-  {
-    value: 80,
-    kind: 3
-  },
-  {
-    value: 80,
-    kind: 4
-  },
-  {
-    value: 220,
-    kind: 5
-  },
-  {
-    value: 110,
-    kind: 6
-  }
-];
+// const data = [
+//   {
+//     value: 200,
+//     kind: 1
+//   },
+//   {
+//     value: 240,
+//     kind: 2
+//   },
+//   {
+//     value: 80,
+//     kind: 3
+//   },
+//   {
+//     value: 80,
+//     kind: 4
+//   },
+//   {
+//     value: 220,
+//     kind: 5
+//   },
+//   {
+//     value: 110,
+//     kind: 6
+//   }
+// ];
 
-const data02 = data.map((obj) => {
-  return {
-    ...obj, categorie:kinds[obj.kind]
-  };
-});
+// const data02 = data.map((obj) => {
+//   return {
+//     ...obj, categorie:kinds[obj.kind]
+//   };
+// });
 
 export default function App(props) {
 
   // console.log(props.data.kind)
-  // let kinds = props.data.kind;
-  // let data01 = props.data.data;
-  // console.log(kinds)
+  let kinds = props.data.kind;
+  let data01 = props.data.data;
+  console.log(kinds)
 
-  // const data02 = data01.map((obj) => {
-  //   return {
-  //     ...obj, categorie:kinds[obj.kind]
-  //   };
-  // });
+  const data02 = data01.map((obj) => {
+    return {
+      ...obj, categorie:kinds[obj.kind]
+    };
+  });
 
   return (
     <RadarChart
